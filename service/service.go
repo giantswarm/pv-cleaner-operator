a94cdcf9eaaa0828bedd0ada5e973511fe9d418a
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-01-09 00:06:47 +0200

    Add operator boilerplate

---
+++ service/service.go
@@ -0,0 +1,138 @@
+package service
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/giantswarm/microendpoint/service/version"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/client/k8srestconfig"
+	"github.com/spf13/viper"
+
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/giantswarm/pv-cleaner-operator/flag"
+)
+
+// Config represents the configuration used to create a new service.
+type Config struct {
+	// Dependencies.
+
+	Logger micrologger.Logger
+
+	// Settings.
+
+	Flag  *flag.Flag
+	Viper *viper.Viper
+
+	Description string
+	GitCommit   string
+	Name        string
+	Source      string
+}
+
+// DefaultConfig provides a default configuration to create a new service by
+// best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		Logger: nil,
+
+		// Settings.
+		Flag:  nil,
+		Viper: nil,
+
+		Description: "",
+		GitCommit:   "",
+		Name:        "",
+		Source:      "",
+	}
+}
+
+// New creates a new configured service object.
+func New(config Config) (*Service, error) {
+	// Dependencies.
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+	config.Logger.Log("debug", fmt.Sprintf("creating pv-cleaner-operator gitCommit:%s", config.GitCommit))
+
+	// Settings.
+	if config.Flag == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Flag must not be empty")
+	}
+	if config.Viper == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Viper must not be empty")
+	}
+
+	var err error
+
+	var restConfig *rest.Config
+	{
+		c := k8srestconfig.DefaultConfig()
+
+		c.Logger = config.Logger
+
+		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
+		c.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
+		c.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
+		c.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
+		c.TLS.KeyFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile)
+
+		restConfig, err = k8srestconfig.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	k8sClient, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	k8sExtClient, err := apiextensionsclient.NewForConfig(restConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var versionService *version.Service
+	{
+		versionConfig := version.DefaultConfig()
+		versionConfig.Description = config.Description
+		versionConfig.GitCommit = config.GitCommit
+		versionConfig.Name = config.Name
+		versionConfig.Source = config.Source
+
+		versionService, err = version.New(versionConfig)
+		if err != nil {
+			return nil, microerror.Maskf(err, "version.New")
+		}
+	}
+
+	newService := &Service{
+		// Dependencies.
+		Version: versionService,
+
+		// Internals
+		bootOnce: sync.Once{},
+	}
+
+	return newService, nil
+}
+
+type Service struct {
+	// Dependencies.
+	Version *version.Service
+
+	// Internals.
+	bootOnce sync.Once
+}
+
+func (s *Service) Boot() {
+	s.bootOnce.Do(func() {
+		s.Operator.Boot()
+	})
+}
