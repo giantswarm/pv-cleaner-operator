5caa0f6242149f52ffcd5c12ae4835584f925abb
Ross Fairbanks <ross@rossfairbanks.com>
2020-01-10 16:05:33 +0100

    Use latest operatorkit wiring

--- service/controller/persistent_volume.go
+++ service/controller/persistent_volume.go
@@ -1,14 +1,12 @@
 package controller
 
 import (
-	"fmt"
-
+	"github.com/giantswarm/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/controller"
-	"github.com/giantswarm/operatorkit/informer"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	v1 "github.com/giantswarm/pv-cleaner-operator/service/controller/v1"
 )
@@ -18,7 +16,7 @@ const (
 )
 
 type PersistentVolumeConfig struct {
-	K8sClient kubernetes.Interface
+	K8sClient k8sclient.Interface
 	Logger    micrologger.Logger
 
 	ProjectName string
@@ -42,24 +40,6 @@ func NewPersistentVolume(config PersistentVolumeConfig) (*PersistentVolume, erro
 		return nil, microerror.Maskf(invalidConfigError, "config.ProjectName must not be empty")
 	}
 
-	var newInformer *informer.Informer
-	{
-		c := informer.Config{
-			Logger:  config.Logger,
-			Watcher: config.K8sClient.CoreV1().PersistentVolumes(),
-
-			ListOptions: metav1.ListOptions{
-				LabelSelector: fmt.Sprintf("%s=%s", cleanupLabel, "true"),
-			},
-			ResyncPeriod: informer.DefaultResyncPeriod,
-		}
-
-		newInformer, err = informer.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
 	var v1ResourceSet *controller.ResourceSet
 	{
 		c := v1.ResourceSetConfig{
@@ -75,27 +55,32 @@ func NewPersistentVolume(config PersistentVolumeConfig) (*PersistentVolume, erro
 		}
 	}
 
-	var operatorkitController *controller.Controller
+	var persistentVolumeController *controller.Controller
 	{
 		c := controller.Config{
-			Informer: newInformer,
-			Logger:   config.Logger,
+			K8sClient: config.K8sClient,
+			Logger:    config.Logger,
+			MatchLabels: map[string]string{
+				cleanupLabel: "true",
+			},
 			ResourceSets: []*controller.ResourceSet{
 				v1ResourceSet,
 			},
-			RESTClient: config.K8sClient.CoreV1().RESTClient(),
+			NewRuntimeObjectFunc: func() runtime.Object {
+				return new(corev1.PersistentVolume)
+			},
 
 			Name: config.ProjectName,
 		}
 
-		operatorkitController, err = controller.New(c)
+		persistentVolumeController, err = controller.New(c)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
 	}
 
 	p := &PersistentVolume{
-		Controller: operatorkitController,
+		Controller: persistentVolumeController,
 	}
 
 	return p, nil
